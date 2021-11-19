99f8350cfaef6cf42eaaf29d59130ddaf3bb9c32
Giovanne Batista <giobatista14@gmail.com>
2021-11-19 06:45:43 -0400

    - Ajustes para deploy no DockerHub

--- SendMailApi/Program.cs
+++ SendMailApi/Program.cs
@@ -5,17 +5,12 @@ using SendGrid.Helpers.Mail;
 using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
